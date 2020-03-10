b5aad0d68fb9803040f5aa88d968be62d3b79ae5
humleflue <humleflue@yahoo.dk>
2020-03-10 22:07:37 +0100

    Now saves the checkbox state in chrome storage

--- BetterMoodleSchedule/content.js
+++ BetterMoodleSchedule/content.js
@@ -2,7 +2,6 @@ chrome.runtime.sendMessage({todo: "showPageAction"});
 addCourseOptions();
 
 function addCourseOptions() {
-    const events = document.getElementsByClassName("event");
     const course_table = document.getElementById("kursustable");
     const options_header = course_table.tHead.rows[0];
     const options_body = course_table.getElementsByTagName('tbody')[0].rows;
@@ -11,31 +10,52 @@ function addCourseOptions() {
     // - Header
     const checkbox_header_cell = options_header.insertCell(0);
     checkbox_header_cell.innerHTML = "Show course";
-    checkbox_header_cell.style = "width: 7%";
+    checkbox_header_cell.setAttribute("id", "show_course");
     // - Body
     for(let i = 0; i < options_body.length; i++){
         const course_name = options_body[i].getElementsByTagName('td')[1].innerText;
-        let checkbox = document.createElement("input");
+        // const reg_exp = /\s(\w+)/;
+        // let course_identifier_arr = reg_exp.exec(course_name);
+        // let course_identifier = course_identifier_arr[1]; // Course identifier is gonna be the first word of the course_name
+        let course_identifier = "course_NO" + i;
 
+        // Create checkbox
+        let checkbox = document.createElement("input");
         checkbox.type = "checkbox";
-        checkbox.checked = true;
         checkbox.classList.add("checkbox");
         options_body[i].getElementsByTagName('td')[0].appendChild(checkbox);
-        checkbox.addEventListener('change', e => {                               // Waits for a change in each checkbox
+        // Get stored state for checkbox
+        chrome.storage.sync.get([course_identifier], result => {
+            if(result[course_identifier] === true){
+                checkbox.checked = result[course_identifier];
+                showOrHideCourse(course_name, 'visible');
+            }
+            else if(result[course_identifier] === false){
+                checkbox.checked = result[course_identifier];
+                showOrHideCourse(course_name, 'hidden');
+            }
+        });
+        
+        // Waits for a change of state in each checkbox
+        checkbox.addEventListener('change', e => {
             if(e.target.checked) {
-                for(let event of events){
-                    if(event.getElementsByTagName("a")[0].text === course_name){
-                        event.style.visibility = 'visible';
-                    }
-                }
+                chrome.storage.sync.set({[course_identifier]: true});
+                showOrHideCourse(course_name, 'visible');
             }
             else {
-                for(let event of events){
-                    if(event.getElementsByTagName("a")[0].text === course_name){
-                        event.style.visibility = 'hidden';
-                    }
-                }
+                chrome.storage.sync.set({[course_identifier]: false});
+                showOrHideCourse(course_name, 'hidden');
             }
-        })
+        });
+    }
+}
+
+function showOrHideCourse(course_name, visibility) {
+    const all_course_events = document.getElementsByClassName("event");
+
+    for(let event of all_course_events){
+        if(event.getElementsByTagName("a")[0].text === course_name){
+            event.style.visibility = visibility;
+        }
     }
 }
