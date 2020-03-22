44902af152768d634c50e05288e42f7f4e96abb5
Jacob <bjoerklundjacob@gmail.com>
2020-03-22 12:04:33 +0100

    Added CSS and alias'
    CSS for events and updates for event visuals through code and added aliases.

--- BetterMoodleSchedule/content.js
+++ BetterMoodleSchedule/content.js
@@ -6,16 +6,105 @@ showAllEventsOfDay(); // User can show all hidden events of a day by clicking on
 changeSpecificEventTime(); // User can change an events time by clicking on it
 getFromChromeStorage(); // Retrieves all values from chrome storage and applies them
 
-// Skifter danebrog ud med et coronaflag på siden FIXME: Fjernes efter corona :D
-replaceDanebrog(); // FIXME: Fjernes efter corona :D
-function replaceDanebrog() { // FIXME: Fjernes efter corona :D
-  const divLanguages = document.getElementById(`languages`); // FIXME: Fjernes efter corona :D
-  const imgElem = divLanguages.childNodes[1].childNodes[0]; // FIXME: Fjernes efter corona :D
-  if (imgElem.src === `https://www.moodle.aau.dk/calmoodle/img/dk.png`) { // FIXME: Fjernes efter corona :D
-    imgElem.src = `https://i.redd.it/cojuelrafcm41.jpg`; // FIXME: Fjernes efter corona :D
-    imgElem.width = `36`; // FIXME: Fjernes efter corona :D
-  } // FIXME: Fjernes efter corona :D
-} // FIXME: Fjernes efter corona :D
+
+innerHTMLReplace(".event", /Time: /, "");//🕒
+innerHTMLReplace(".event", /Location: /, "");//📍
+innerHTMLReplace(".event", /Note: /, "");//📄
+addCourseAliasInputs();
+updateCourseNames();
+
+function updateCourseNames(){
+  const courseTable = document.querySelectorAll("#kursustable tr");
+
+  for(let i = 1; i < courseTable.length; ++i){
+    let course = courseTable[i];
+    let alias = course.querySelector("td+td+td>input").value;
+
+    updateCourseName(course.querySelector("td+td").innerHTML, alias);
+  }
+}
+
+function updateCourseName(courseName, alias){
+  const classNames = document.querySelectorAll("div.event a.courseName");
+  for(let className of classNames){
+    if(className.innerHTML === courseName){
+      className.nextSibling.innerHTML = alias;
+    }
+  }
+}
+
+function innerHTMLReplace(element, search, replace){
+  if(typeof element === "string"){
+    element = document.querySelectorAll(element);
+    for(let el of element){
+      el.innerHTML = el.innerHTML.replace(search, replace);
+    }
+  }
+  else{
+    element.innerHTML = element.innerHTML.replace(search, replace);
+  }
+}
+
+function addCourseAliasInputs(){
+  let courseRows = document.querySelectorAll("table#kursustable tr");
+  
+  //Start from 1 to avoid the "Courses" row
+  for(let i = 1; i < courseRows.length; ++i){
+    //Get the course row
+    let tr = courseRows[i];
+    //Create new table cell with a text input element inside
+    let td = document.createElement("td");
+    td.innerHTML = "<input type=\"text\">";
+    let courseName = tr.childNodes[1].innerHTML;
+    let courseAlias = courseName.match(/\([A-Z]+\)/);
+    td.childNodes[0].value = courseAlias ? courseAlias[0].match(/[A-Z]+/)[0] : courseName.match(/[a-zA-Z]+/g)[1];
+    //Add new table cell at the end of the row
+    tr.appendChild(td);
+
+    //Get the saved alias' and update when loaded if they exist
+    chrome.storage.sync.get([courseName+"_alias"], (result) => {
+      if(Object.keys(result).length > 0){
+        let key = Object.keys(result)[0];
+        let courseRows = document.querySelectorAll("table#kursustable tr");
+        console.log(key);
+        //Start from 1 to avoid the "Courses" row
+        for(let i = 1; i < courseRows.length; ++i){
+          //Get the course row
+          let tr = courseRows[i];
+          //If it is the matching course
+          if(key.indexOf(tr.childNodes[1].innerHTML) !== -1){
+            //Update alias input and course name
+            tr.childNodes[2].childNodes[0].value = result[key];
+            updateCourseName(tr.childNodes[1].innerHTML, result[key]);
+            break;
+          } 
+        }
+      }
+    });
+  }
+
+  const classNames = document.querySelectorAll("div.event a");
+  for(let className of classNames){
+    //Insert new a after original a
+    className.parentNode.insertBefore(document.createElement("a"), className.nextSibling);
+    className.nextSibling.setAttribute("href", className.getAttribute("href"));
+    className.nextSibling.setAttribute("class", "alias");
+    className.setAttribute("class", "courseName");
+    
+    //Hide original <a> used for searching for courses when hidden/showing
+    className.style.display = "none";
+  }
+
+  //Add event listener to alias input field to update course names if updated
+  for(let aliasInput of document.querySelectorAll("td+td+td>input")){
+    aliasInput.addEventListener("input", (event) => {
+      updateCourseName(event.target.parentNode.previousSibling.innerHTML, event.target.value);
+      //Save new alias
+      chrome.storage.sync.set({ [`${event.target.parentNode.previousSibling.innerHTML}_alias`]: event.target.value });
+    });
+  }
+}
+
 
 /* ***************************** ADDCOURSEOPTIONS ****************************** */
 function addCourseOptions() {
